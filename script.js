dff5da78fbcaab6519070119d057484496d9b3ef
Fadlin <fadlin.rizqi3216@gmail.com>
2024-11-11 20:45:22 +0700

    Update html and javascript files

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
-const cat = {
-    color: "white",
-    habit: function(name){
-        console.log(`${name}'s device`, this);
-    },
-}
-console.log(this);
-cat.habit("linlin");
+console.log("Hello World");
+
